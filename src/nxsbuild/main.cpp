2da25f483eb12d68c4d3b52e206e18f156cfde9c
Jacopo Del Granchio <jacopo@delgranchio.it>
2026-09-21 19:08:31 +0200

    fix: off by one in `-f` flag help text

--- src/nxsbuild/main.cpp
+++ src/nxsbuild/main.cpp
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]) {
 		output += ".nxs";
 
 	if(node_size < 1000 || node_size >= 1<<16) {
-		cerr << "Patch size (" << node_size << ") out of bounds [1000-65536]" << endl;
+		cerr << "Patch size (" << node_size << ") out of bounds [1000-65535]" << endl;
 		return -1;
 	}
 
